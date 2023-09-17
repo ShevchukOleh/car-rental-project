94153457b3352256069c06ebf6d9b190fa28c479
ShevchukOleh <shew4ukoleg12@gmail.com>
2023-09-17 22:28:33 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 import { Catalog } from "pages/Catalog/Catalog";
 import { Favorites } from "pages/Favorites/Favorites";
 import Home from "pages/Home/Home";
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import Layout from "./Layout/Layout";
 
 function App() {
@@ -10,7 +10,8 @@ function App() {
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
         <Route path="/catalog" element={<Catalog />} />
-        <Route path="/favorites" element={<Favorites />} />
+        <Route path="/favorites" element={<Favorites />} />        
+        <Route path="*" element={<Navigate to="/" />} />
       </Route>
     </Routes>
   );
